33ac1cb6017a8b2c7dd5b3ea61c4a3c3e4c41f20
kokomi <141301585+KokomiServer@users.noreply.github.com>
2023-11-15 21:59:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //整活
-window.location.replace('https://www.bilibili.com/video/BV16h4y1d7rP');
-window.open('https://www.bilibili.com/video/BV16h4y1d7rP','_blank');
+window.location.replace('https://www.bilibili.com/video/BV1EM411Q7pS');
+window.open('https://www.bilibili.com/video/BV1EM411Q7pS','_blank');
+//window.location.replace('https://www.bilibili.com/video/BV16h4y1d7rP');
+//window.open('https://www.bilibili.com/video/BV16h4y1d7rP','_blank');
 setInterval(
     function(){
     var body=document.getElementById('body');
